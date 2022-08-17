bd5d1c81772643c494f676849e207ec52d0d4339
Connor Mullan <cmullan602@gmail.com>
2022-08-16 20:57:15 -0700

    deleted helpers

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -2,36 +2,6 @@ const fs = require('fs');
 const util = require('util');
 const path = require('path');
 
-// Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
-};
-
 
 
 const createNote = (body, notes) => {
@@ -50,4 +20,4 @@ const createNote = (body, notes) => {
       return note
 }
 
-module.exports = { readFromFile, writeToFile, readAndAppend, createNote};
+module.exports = { createNote};
